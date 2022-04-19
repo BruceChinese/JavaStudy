b3595a19edc2302ada234ddebb7da74b9bf92461
Bruce <lungta2013@outlook.com>
2022-04-19 23:12:07 +0800

    20220419

---
+++ src/base/TestVersion01.java
@@ -0,0 +1,11 @@
+package base;
+
+public class TestVersion01 {
+    public static void main(String[] args) {
+        int age;
+        age = 10;
+        age =20;
+        age = 24;
+        System.out.println(age);
+    }
+}
